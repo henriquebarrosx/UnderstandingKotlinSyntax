beef7344d7ed33e273688802905a69a4b106f028
Henrique Barros <henriquexavierb@gmail.com>
2021-04-28 21:13:29 -0300

    Working with multi match in when conditional

--- src/main/kotlin/Conditionals.kt
+++ src/main/kotlin/Conditionals.kt
@@ -1,19 +1,20 @@
 import objects.Color
 
 fun main() {
-    println("Normal Function: ${ sum1(3, 5) }");
     println("Short Function: ${sum2(3, 5)}");
+    println("Block Function: ${ sum1(3, 5) }");
+
     println("Color: ${getColorStr(Color.BLUE)}");
+    println("Color: ${getColorTemperatureStr(Color.BLUE)}");
 }
+
+// Expression body with inline conditional
+fun sum2(x: Int, y: Int): Int = if (x > 3) x + y else x * y;
 
 // Block body
 fun sum1(x: Int, y: Int): Int {
-    // Inline conditional
     return if (x > 3) x + y else x * y;
 }
-
-// Expression body
-fun sum2(x: Int, y: Int): Int = if (x > 3) x + y else x * y;
 
 // Conditionals using enum
 fun getColorStr(color: Color): String {
@@ -23,5 +24,14 @@ fun getColorStr(color: Color): String {
         Color.GREEN -> "Green"
         Color.ORANGE -> "Orange"
         Color.YELLOW -> "Yellow"
+    }
+}
+
+// Conditionals using enum multi match
+fun getColorTemperatureStr(color: Color): String {
+    return when(color) {
+        Color.RED, Color.ORANGE, Color.YELLOW -> "Hot"
+        Color.BLUE -> "Neutral"
+        Color.GREEN -> "Cool"
     }
 }
